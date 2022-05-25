e981d3932e0efced17e1cb7639f4cfe21dfb6714
SavinaRuxandra <savina_ruxandra@yahoo.com>
2022-05-26 01:23:54 +0300

    try to deploy

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/blockchain-app'),
+      dir: require('path').join(__dirname, './coverage/lets-be-human'),
       subdir: '.',
       reporters: [
         { type: 'html' },
